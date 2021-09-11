7b80d4ae0a250249537968ba059650534b26ad69
Justin Rockwood <justinrockwood@hotmail.com>
2021-09-10 17:46:32 -0700

    Add an index default export file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { ColoredConsole, ColoredConsoleOptions, defaultLogTypePrefixes, LogType } from './ColoredConsole';
+export { Color } from './ConsoleColors';
+export { ConsoleErrorReporter, ErrorReporter } from './ErrorReporter';
+export { OsColoredConsole } from './OsColoredConsole';
+export { ColoredLine, VirtualColoredConsole } from './VirtualColoredConsole';
